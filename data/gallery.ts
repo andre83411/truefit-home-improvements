877fae3907ece5bf4a930523aa833040bb96bb3f
andre83411 <andreamontes.rei@gmail.com>
2026-07-23 18:59:14 -0400

    Replace stair flooring gallery image

--- data/gallery.ts
+++ data/gallery.ts
@@ -45,8 +45,8 @@ export const galleryProjects = [
     description:
       "A full stair refresh with coordinated wood-look treads and risers.",
     image: "/gallery/airbnb-stairs-flooring-before-after.png",
-    width: 1024,
-    height: 1536,
+    width: 1800,
+    height: 2200,
     alt: "Before and after comparison of stairs finished with wood-look treads and risers",
   },
   {
